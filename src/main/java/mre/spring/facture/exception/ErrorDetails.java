06b46926f25775e1ec59c9bb71796613a14c8d26
max <ren.maxime@gmail.com>
2020-12-11 15:18:10 +0100

    close #13

--- src/main/java/mre/spring/facture/exception/ErrorDetails.java
+++ src/main/java/mre/spring/facture/exception/ErrorDetails.java
@@ -3,13 +3,13 @@ package mre.spring.facture.exception;
 import java.util.Date;
 
 public class ErrorDetails {
-    private Date timestamp;
+    private Date dayDate;
     private String message;
     private String details;
 
     public ErrorDetails(Date timestamp, String message, String details) {
         super();
-        this.timestamp = timestamp;
+        this.dayDate = timestamp;
         this.message = message;
         this.details = details;
     }
